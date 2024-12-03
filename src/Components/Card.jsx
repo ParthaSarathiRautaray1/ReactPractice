2e84649d3db5604a709a79855dd5fbf03e93233a
ParthaSarathiRautaray1 <145197547+ParthaSarathiRautaray1@users.noreply.github.com>
2024-12-03 09:29:48 +0530

    conditional rendering done

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -8,18 +8,21 @@ function Card() {
       name: "Amazon Basics.",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus",
+      stock: true,
     },
     {
       image:
         "https://plus.unsplash.com/premium_photo-1664970900401-0756aa4d8459?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGRhaWx5JTIwb2JqZWN0c3xlbnwwfHwwfHx8MA%3D%3D",
       name: "Daily Objects",
       description: "Lorem ipsum dolor sit amet consectet",
+      stock: false,
     },
     {
       image:
         "https://plus.unsplash.com/premium_photo-1663926032043-7908766f5c52?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGFwcGxlfGVufDB8fDB8fHww",
       name: "APPLE",
       description: "Lorem ipsum dolor sit amet consectet",
+      stock: true,
     },
   ];
   return (
@@ -38,6 +41,9 @@ function Card() {
           <div className="w-full px-3 py-4">
             <h2 className="font-semibold">{e.name}</h2>
             <p className="text-xs mt-5">{e.description}</p>
+            <button className={`mt-3 px-4 py-1 text-white text-sm ${e.stock ? "bg-blue-400" : "bg-red-400"} rounded font-normal`}>
+              {e.stock ? `In Stock` : `Out of Stock`}
+            </button>
           </div>
         </div>
       ))}
